691f1fc929650aad13b69ee5d051d8b970557c3f
Arun-D <arund@dckap.com>
2019-02-13 11:41:45 +0530

    initial commit

---
+++ assets/application.js
@@ -0,0 +1,18 @@
+// Put your applicaiton javascript here
+
+// $(document).ready(function() {
+//   cosole.log('dddddddd');
+//   var sections = new theme.Sections();
+
+//   sections.register('product-temp', theme.Product);
+//   sections.register('collection-template', theme.Collection);
+//   sections.register('footer-section', theme.FooterSection);
+//   sections.register('featured-content-section', theme.FeaturedContentSection);
+//   sections.register('newsletter-section', theme.NewsletterSection);
+//   sections.register('sidebar-section', theme.SidebarSection);
+//   sections.register('slideshow-section', theme.SlideshowSection);
+//   sections.register('password-content', theme.PasswordContent);
+//   sections.register('map', theme.Maps);
+//  });
+
+// $(theme.init);
